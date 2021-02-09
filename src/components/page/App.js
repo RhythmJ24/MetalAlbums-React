bfc8aa7e3a90cfc884e2812080ada26ec29a98e2
Rhythm Jadeja <rhythjadej@student.mitt.ca>
2021-02-09 06:30:31 +0530

    add form elements and css styling

--- src/components/page/App.js
+++ src/components/page/App.js
@@ -18,24 +18,45 @@ class App extends Component{
     return(
       <div className="wrapper">
         <p className="heading">Popular Metal ALbums</p>
-        <div>
-          <img src="https://images.unsplash.com/photo-1598387846148-47e82ee120cc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80" alt="album-iamge"/>
-          <div>
-            <span>
-              &#9650;
-            </span>
-            <span>2</span>
-          </div>
-          <a href=''>Batman</a>
-          <p>Marvel, 2019</p>
-          <div className="avatar-block">
-            <p>Submitted by</p>
-            <img src="https://i.ibb.co/JkH8ch2/avataaars.png" alt="avtar-image"/>
+        <div className="album">
+          <img className="image-album-class" src="https://images.unsplash.com/photo-1598387846148-47e82ee120cc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80" alt="album-iamge"/>
+          <div className="album-right">
+            <div className="vote">
+              <span className="arrow-up">
+                &#9650;
+              </span>
+              <span>2</span>
+            </div>
+            <a className="album-name" href=''>Batman</a>
+            <p className="band-name">Marvel, 2019</p>
+            <div className="avatar-block">
+              <p className="submitted-by">Submitted by</p>
+              <img className="aprofile" src="https://i.ibb.co/JkH8ch2/avataaars.png" alt="avtar-image"/>
+            </div>
           </div>
         </div>
         <div>
+          <p className="add-new">Add New</p>
           <form>
-            
+            <div>
+            <input className="input" type="text" name="album_name" placeholder="Album Name" />
+            </div>
+             <div>
+             <input className="input" type="text" name="owned_by" placeholder="Band Name" />
+             </div>
+             <div>
+             <input className="input" type="text" name="release_year" placeholder="Release Year" />
+             </div>
+             <div>
+             <input className="input" type="text" name="album_url" placeholder="Album Cover URL" />
+             </div>
+            <input className="input" type="text" name="more_info" placeholder="More Info URL" />
+            <div>
+              <select className="select" name="submitted_by" placeholder="Submitted by">
+                <option>Rhythm</option>
+                <option>Maxx</option>
+              </select>
+            </div>
           </form>
         </div>
       </div>
